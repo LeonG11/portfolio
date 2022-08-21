7ee2ab3f7b521385745e63cc69210c4a9903e226
Kirill Kachanov <kkacanov198@gmail.com>
2022-08-21 23:00:37 +0300

    Start project, delete standard selector
    1. Add in project: fontawesome for add arrow and other icons on pages
    2. Add in project: sass for compile and read file in vite-react project
    3. Start make structure of project, all project will made on flexbox,
       because it's free road to make simple project
    4. Add scss file instead of css, because can be used class and many
       other stuff
    5. TODO: need
    5.1 Edit photo and paste on page
    5.2 Need start make mixin for interactive site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
   return (
-    <div></div>
-  )
+    <div className="portfolio">
+      <div className="portfolio-nav">
+        <div className="porfolio-name">Kirill Kachanov</div>
+        <div className="portfolio-profession">Front end engineer</div>
+        <div className="portfolio-based">
+          Based in SPb
+          <br />
+          Russian Federation
+        </div>
+        <div className="portfolio-work">Work, about contact</div>
+      </div>
+      <div className="portfolio-main">
+        <h1>
+          creative <br /> developer
+        </h1>
+        <div className="portfolio-down">
+          <FontAwesomeIcon className="portfolio-arrow" icon={faArrowDown} />
+          <div className="portfolio-describe">
+            Developer from Urals , live in Saint-Peterburg and make cool stuff
+          </div>
+          <h1>
+            kirill <br /> kachanov
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
