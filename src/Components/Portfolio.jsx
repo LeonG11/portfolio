a5aade28a87f78a93e5f28efb9eff5c86cd9fdd5
Kirill Kachanov <kkacanov198@gmail.com>
2022-08-25 10:49:37 +0300

    Break app on many file

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { faArrowDown } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Portfolio() {
+  return (
+    <div className="portfolio">
+      <div className="portfolio-nav">
+        <div className="porfolio-name">Kirill Kachanov</div>
+        <div className="portfolio-profession">Front end engineer</div>
+        <div className="portfolio-based">
+          Based in SPb
+          <br />
+          Russian Federation
+        </div>
+        <div className="portfolio-work">Work, about contact</div>
+      </div>
+      <div className="portfolio-main">
+        <h1>
+          creative <br /> developer
+        </h1>
+        <div className="portfolio-down">
+          <FontAwesomeIcon className="portfolio-arrow" icon={faArrowDown} />
+          <div className="portfolio-describe">
+            Developer from Urals , live in Saint-Peterburg and make cool stuff
+          </div>
+          <h1>
+            kirill <br /> kachanov
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
