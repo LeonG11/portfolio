c001b5e1335193821cfe3fa076a3d5055c8066ec
Kirill Kachanov <kkacanov198@gmail.com>
2022-08-25 12:33:52 +0300

    Add work part, add description

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from "react";
+import wave from "./images/wave.jpg";
 
 export default function Works(props) {
   return (
-    <div>
-    <h1>my work</h1>
+    <div className="works">
+      <div className="works__flex">
+        <div className="works__left">
+          <div className="works__smile">👾</div>
+          <div className="works__title">About me</div>
+          <div className="works__text">
+            Hi, im Kirill, a frontend developer, based in Saint-Peterburgs.
+            Russia. I loved work with little project and use in work new
+            technology and many other crazy stuff. At this moment i am looking
+            for new project/product on long time.
+          </div>
+        </div>
+        <div className="works__middle">
+          <img src="" alt="" />
+        </div>
+        <div className="works__right">
+          <img src={wave} />
+          <div className="works__title">FishFish</div>
+          <div className="works__text">
+            On this time it is best work from all, and i can proud to say he
+            deserves a sequel. You can also take a closer look at my work on
+          </div>
+          <button className="works__button github">github</button>
+          <button className="works__button vk">behance</button>
+        </div>
+      </div>
     </div>
   );
-  
 }
